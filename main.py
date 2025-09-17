a08e13f81199dc7a2d611ab5e12eb7b8c8443e55
Khwaish Jain <jainkhwaish554@gamil.com>
2025-09-18 01:48:14 +0530

    The commited main.py file

---
+++ main.py
@@ -0,0 +1,37 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+try:
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(f"the addition of {num1} and {num2} is {add(num1,num2)}")
+    if choice =="2":
+        print(f"the subtraction of {num1} and {num2} is {subtract(num1,num2)}")
+    if choice =="3":
+        print(f"the multiplication of {num1} and {num2} is {multiply(num1,num2)}")
+    if choice =="4":
+        print(f"the divsion of {num1} and {num2} is {divide(num1,num2)}")
+except ValueError:
+    print("Please Enter the valid number")
